0b5ab5ade3526ae2e4ff042c3db1630978febba2
Curtis Gray <curtis@carverlab.com>
2024-03-18 13:59:51 -0400

    Add error reporting for wingman launch

--- wingman/wingman_launcher.cpp
+++ wingman/wingman_launcher.cpp
@@ -33,6 +33,33 @@ namespace wingman {
 
 	bool requested_shutdown;
 
+#ifdef _WIN32
+	//Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+	std::string GetLastErrorAsString()
+	{
+		//Get the error message ID, if any.
+		DWORD errorMessageID = ::GetLastError();
+		if (errorMessageID == 0) {
+			return std::string(); //No error message has been recorded
+		}
+
+		LPSTR messageBuffer = nullptr;
+
+		//Ask Win32 to give us the string version of that message ID.
+		//The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+		size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+									 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+		//Copy the error message into a std::string.
+		std::string message(messageBuffer, size);
+
+		//Free the Win32's string's buffer.
+		LocalFree(messageBuffer);
+
+		return message;
+	}
+#endif
+
 	// ReSharper disable once CppInconsistentNaming
 	void SIGINT_Callback(const int signal)
 	{
@@ -208,29 +235,38 @@ namespace wingman {
 
 		TinyProcessLib::Process serverProcess(
 			std::vector<std::string>
-			{
-				"wingman",
-					"--port",
-					std::to_string(port),
-					"--websocket-port",
-					std::to_string(websocketPort),
-					"--gpu-layers",
-					std::to_string(gpuLayers),
-			},
+		{
+			"wingman",
+				"--port",
+				std::to_string(port),
+				"--websocket-port",
+				std::to_string(websocketPort),
+				"--gpu-layers",
+				std::to_string(gpuLayers),
+		},
 			fs::current_path().string(),
 			[](const char *bytes, size_t n) {
 				// spdlog::debug("Wingman: {}", util::stringRightTrimCopy(std::string(bytes, n)));
-				std::cout << "Wingman: " << std::string(bytes, n);
-			},
+			std::cout << "Wingman: " << std::string(bytes, n);
+		},
 			[](const char *bytes, size_t n) {
 				// spdlog::debug("Wingman (stderr): {}", util::stringRightTrimCopy(std::string(bytes, n)));
-				const auto str = std::string(bytes, n);
-				if (str == ".")
-					std::cerr << str;
-				else
-					std::cerr << "Wingman (stderr): " << std::string(bytes, n);
-			}
+			const auto str = std::string(bytes, n);
+			if (str == ".")
+				std::cerr << str;
+			else
+				std::cerr << "Wingman (stderr): " << std::string(bytes, n);
+		}
 		);
+
+#ifdef _WIN32
+		auto lastError = GetLastError();
+		if (lastError != 0) {
+			auto lastErrorString = GetLastErrorAsString();
+			spdlog::debug("GetLastError: {}, GetLastErrorAsString: {}", lastError, lastErrorString);
+			return lastError;
+		}
+#endif
 
 		shutdown_handler = [&](int /* signum */) {
 			spdlog::debug("SIGINT received. Attempting to shutdown Wingman server gracefully...");
